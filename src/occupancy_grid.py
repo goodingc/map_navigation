a5d24efea0b574ca6054a4f64aaf02f21ed7162b
Oliver Gledhill <psyorg@nottingham.ac.uk>
2020-11-25 17:05:40 +0000

    Corrected comments in to_grid and to_world + started to_grid

--- src/occupancy_grid.py
+++ src/occupancy_grid.py
@@ -17,18 +17,26 @@ class OccupancyGrid:
         @type size: Tuple[int, int]
         @type resolution: float
         """
+        origin = self.origin
+        size = self.size
+        resolution = self.resolution
+
+
 
     def to_grid(self, world_x, world_y):
         """
-        Converts grid coordinates to world coordinates, or None if world coordinates are out of bound
+        Converts world coordinates to grid coordinates, or None if world coordinates are out of bound
         @type world_x: float
         @type world_y: float
         @rtype: Optional[Tuple[int, int]]
         """
+        if world_x > self.size[0] or world_y > self.size[1] or world_x < 0 or world_y < 0:
+            return None
+
 
     def to_world(self, grid_x, grid_y):
         """
-        Converts world coordinates to grid coordinates, or None if converted coordinates are out of bounds
+        Converts grid coordinates to world coordinates, or None if converted coordinates are out of bounds
         @author Callum
         @type grid_x: int
         @type grid_y: int
